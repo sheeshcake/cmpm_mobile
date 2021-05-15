192d8ffe6e2b20127474057819972f60fc35aac5
wendale1231 <spooderm5@gmail.com>
2021-05-15 19:18:04 +0800

    UPDATE **added CE and PM functions **bug fix in space time continuum

--- www/js/global.js
+++ www/js/global.js
@@ -1,2 +1,2 @@
-var host = "http://192.168.1.8:8080/api/";
-var host_non_api = "http://192.168.1.8:8080/";
+var host = "http://192.168.1.8:8000/api/";
+var host_non_api = "http://192.168.1.8:8000/";
